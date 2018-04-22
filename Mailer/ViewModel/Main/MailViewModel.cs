045c485ea820380eef285976c84996390b592c3d
Donni <pet9_96@inbox.ru>
2018-04-22 19:23:17 +0300

    commit

--- Mailer/ViewModel/Main/MailViewModel.cs
+++ Mailer/ViewModel/Main/MailViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms.VisualStyles;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 using MailBee.ImapMail;
@@ -22,10 +23,16 @@ namespace Mailer.ViewModel.Main
         private List<FolderExtended> _foldersExtended;
         private int _selectedFolder;
         private bool _isMessagesLoading;
+        private bool _isMessageFormVisible;
+        private bool _isMessageLoading;
+        private MailMessage _message;
+
 
         public RelayCommand GoToSettingsCommand { get; private set; }
         public RelayCommand AddFolderCommand { get; private set; }
         public RelayCommand<FolderExtended> DeleteFolderCommand { get; private set; }
+        public RelayCommand<MailMessage> ReadEmailCommand { get; private set; }
+        public RelayCommand CloseMessageCommand { get; private set; }
 
         public MailViewModel()
         {
@@ -45,6 +52,8 @@ namespace Mailer.ViewModel.Main
             });
             AddFolderCommand = new RelayCommand(CreateFolder);
             DeleteFolderCommand = new RelayCommand<FolderExtended>(DeleteFolder);
+            ReadEmailCommand = new RelayCommand<MailMessage>(ReadEmail);
+            CloseMessageCommand = new RelayCommand(CloseMessage);
         }
 
         public int SelectedFolder
@@ -75,6 +84,24 @@ namespace Mailer.ViewModel.Main
             set => Set(ref _isMessagesLoading, value);
         }
 
+        public bool IsMessageFormVisible
+        {
+            get => _isMessageFormVisible;
+            set => Set(ref _isMessageFormVisible, value);
+        }
+
+        public bool IsMessageLoading
+        {
+            get => _isMessageLoading;
+            set =>  Set(ref _isMessageLoading, value);
+        }
+
+        public MailMessage Message
+        {
+            get => _message;
+            set => Set(ref _message, value);
+        }
+
         public MailMessageCollection MailMessageCollection => _foldersExtended == null ? null : FoldersExtended[SelectedFolder].MailMessageCollectionReversed;
 
         private async void LoadInfo()
@@ -99,8 +126,8 @@ namespace Mailer.ViewModel.Main
                 int msgCount = ViewModelLocator.ImapClient.MessageCount > 50 ? 50 : ViewModelLocator.ImapClient.MessageCount;
                 int firstIndex = ViewModelLocator.ImapClient.MessageCount - msgCount + 1;
                 int lastIndex = ViewModelLocator.ImapClient.MessageCount;
-                //FoldersExtended[folderIndex].MailMessageCollection = await ViewModelLocator.ImapClient.DownloadMessageHeadersAsync(firstIndex + ":" + lastIndex, false);
-                FoldersExtended[folderIndex].MailMessageCollection = await ViewModelLocator.ImapClient.DownloadMessageHeadersAsync(Imap.AllMessages, false);
+                FoldersExtended[folderIndex].MailMessageCollection = await ViewModelLocator.ImapClient.DownloadMessageHeadersAsync(firstIndex + ":" + lastIndex, false);
+                //FoldersExtended[folderIndex].MailMessageCollection = await ViewModelLocator.ImapClient.DownloadMessageHeadersAsync(Imap.AllMessages, false);
                 //var test = FoldersExtended[0].MailMessageCollection[0];
                 RaisePropertyChanged("MailMessageCollection");
             }
@@ -146,6 +173,22 @@ namespace Mailer.ViewModel.Main
         {
             await ViewModelLocator.ImapClient.DeleteFolderAsync(folder.Name);
             LoadInfo();
+        }
+
+        public async void ReadEmail(MailMessage message)
+        {
+            IsMessageLoading = true;
+            IsMessageFormVisible = true;
+            var fullMessage = await ViewModelLocator.ImapClient.DownloadEntireMessageAsync(Convert.ToInt64(message.UidOnServer), true);
+            if (fullMessage.BodyPlainText == "")
+                fullMessage.MakePlainBodyFromHtmlBody();
+            Message = fullMessage;
+            IsMessageLoading = false;
+        }
+
+        public void CloseMessage()
+        {
+            IsMessageFormVisible = false;
         }
 
     }
